da4e0df519a4f7c80b899e9e8343ca6dcfce21b1
山崎ヒカル <chris@csie.io>
2024-06-12 18:44:31 +0800

    resolve issue with special cases (#1)

--- ParenthesesRemoval/script.pl
+++ ParenthesesRemoval/script.pl
@@ -11,3 +11,9 @@ print ParenthesesRemoval::remove_parentheses('1*(2+(3*(4+5)))'), "\n";
 print ParenthesesRemoval::remove_parentheses('2 + (3 / -5)'), "\n";
 # f("x+(y+z)+(t+(v+w))") ===> "x+y+z+t+v+w"
 print ParenthesesRemoval::remove_parentheses('x+(y+z)+(t+(v+w))'), "\n";
+print ParenthesesRemoval::remove_parentheses('(2*(3+4)*5)/6'), "\n";
+print ParenthesesRemoval::remove_parentheses('(-5)/7'), "\n";
+print ParenthesesRemoval::remove_parentheses('(-5)*7'), "\n";
+print ParenthesesRemoval::remove_parentheses('1+(-1)'), "\n";
+print ParenthesesRemoval::remove_parentheses('5*(-3)'), "\n";
+print ParenthesesRemoval::remove_parentheses('((2*((2+3)-(4*6))+(8+(7*4))))'), "\n";
