d70f553c67d3e19f55d77f178284e329936b33ed
chris <chris@csie.io>
2024-06-12 18:43:23 +0800

    resolve issue with special cases

--- ParenthesesRemoval/lib/ParenthesesRemoval.pm
+++ ParenthesesRemoval/lib/ParenthesesRemoval.pm
@@ -3,103 +3,136 @@ package ParenthesesRemoval;
 use strict;
 use warnings;
 
-# Fill the operator positions array
-sub fill_operator_positions {
-    my ($characters, $length, $direction) = @_;
-    my @operator_positions = (-1) x ($length + 1);
-    my $last_operator = -1;
+# Define operators
+my @operators = ('*', '+', '-', '/');
 
-    my ($start, $end, $step) = $direction eq 'start' ? (0, $length, 1) : ($length - 1, -1, -1);
+# Update the position of the last seen operator
+sub set_last_operator_position {
+    my ($expression_chars, $index, $operator_positions, $last_operator_ref) = @_;
+    $operator_positions->[$index] = $$last_operator_ref;
 
-    for (my $i = $start; $i != $end; $i += $step) {
-        $operator_positions[$i] = $last_operator;
-        if ($characters->[$i] =~ /[\+\-\*\/]/) {
-            $last_operator = $characters->[$i];
-        }
+    if (grep { $_ eq $expression_chars->[$index] } @operators) {
+        $$last_operator_ref = $expression_chars->[$index];
     }
-
-    return @operator_positions;
 }
 
-# Check if parentheses can be removed
-sub can_remove_parentheses {
-    my ($start, $end, $last_operator_start, $next_operator_end, $operator_presence, $characters, $length) = @_;
+# Fill the operator positions array for last and next operators
+sub fill_operator_positions {
+    my ($expression_chars) = @_;
+    my $length = scalar @$expression_chars;
+    my @last_operator_positions = (-1) x ($length + 1);
+    my @next_operator_positions = (-1) x ($length + 1);
+    my ($last_seen_operator, $next_seen_operator) = (-1, -1);
 
-    my $next_operator = $next_operator_end->[$end];
-    my $last_operator = $last_operator_start->[$start];
+    for my $i (0 .. $length - 1) {
+        set_last_operator_position($expression_chars, $i, \@last_operator_positions, \$last_seen_operator);
+        set_last_operator_position($expression_chars, $length - 1 - $i, \@next_operator_positions, \$next_seen_operator);
+    }
 
-    # Conditions to remove parentheses
-    return 1 if ($start > 0 && $end + 1 < $length && $characters->[$start - 1] eq '(' && $characters->[$end + 1] eq ')');
-    return 1 if (!grep { $operator_presence->{$_} } keys %$operator_presence);
-    return 1 if ($last_operator eq -1 && $next_operator eq -1);
-    return 0 if ($last_operator eq '/' || ($last_operator eq '-' && ($operator_presence->{'+'} || $operator_presence->{'-'})));
-    return 1 if (!grep { $operator_presence->{$_} } ('+', '-'));
-    return 1 if (($last_operator eq -1 || $last_operator =~ /[\+\-]/) && ($next_operator eq -1 || $next_operator =~ /[\+\-]/));
-
-    return 0;
+    return (\@last_operator_positions, \@next_operator_positions);
 }
 
 # Process parentheses in the expression
 sub process_parentheses {
-    my ($characters, $length, $last_operator_start, $next_operator_end) = @_;
-    my @parentheses_stack;
-    my @keep = (1) x ($length + 1);
-    my %operator_presence;
-    my @operators = ('*', '+', '-', '/');
-    my @last_operator_position = (-1) x 256;
+    my ($expression_chars, $last_operator_positions_ref, $next_operator_positions_ref) = @_;
 
-    for (my $i = 0; $i < $length; $i++) {
-        foreach my $operator (@operators) {
-            $operator_presence{$operator} = 0;
-            if ($operator eq $characters->[$i]) {
-                $last_operator_position[ord($operator)] = $i;
-            }
+    my $length = scalar @$expression_chars;
+    my @stack;
+    my @operator_positions = (-1) x 256;
+    my @operator_map = (0) x 256;
+    my @last_operator_positions = @$last_operator_positions_ref;
+    my @next_operator_positions = @$next_operator_positions_ref;
+    my @keep_parentheses = (1) x $length;
+
+    for my $pos (0 .. $length - 1) {
+        update_operator_positions(\@operator_map, \@operator_positions, $expression_chars->[$pos], $pos);
+
+        if ($expression_chars->[$pos] eq '(') {
+            push @stack, $pos;
         }
-
-        if ($characters->[$i] eq '(') {
-            push @parentheses_stack, $i;
-        } elsif ($characters->[$i] eq ')') {
-            my $start = pop @parentheses_stack;
-            my $end = $i;
-
-            foreach my $operator (@operators) {
-                if ($last_operator_position[ord($operator)] >= $start) {
-                    $operator_presence{$operator} = 1;
-                }
-            }
-
-            if (can_remove_parentheses($start, $end, $last_operator_start, $next_operator_end, \%operator_presence, $characters, $length)) {
-                $keep[$start] = 0;
-                $keep[$end] = 0;
+        elsif ($expression_chars->[$pos] eq ')') {
+            my $start = pop @stack;
+            if (can_remove_parentheses(\@operator_map, \@operator_positions, $expression_chars, $start, $pos, \@last_operator_positions, \@next_operator_positions)) {
+                $keep_parentheses[$start] = 0;
+                $keep_parentheses[$pos] = 0;
             }
         }
     }
+    return \@keep_parentheses;
+}
 
-    return @keep;
+# Update operator positions based on the current character
+sub update_operator_positions {
+    my ($operator_map, $operator_positions, $char, $pos) = @_;
+    for my $operator (@operators) {
+        $operator_map->[ord($operator)] = 0;
+        $operator_positions->[ord($operator)] = $pos if $operator eq $char;
+    }
+}
+
+# Determine if parentheses can be removed
+sub can_remove_parentheses {
+    my ($operator_map, $operator_positions, $expression_chars, $start, $end, $last_operator_positions, $next_operator_positions) = @_;
+
+    for my $operator (@operators) {
+        if ($operator_positions->[ord($operator)] >= $start) {
+            $operator_map->[ord($operator)] = 1;
+        }
+    }
+
+    my $next_operator = $next_operator_positions->[$end];
+    my $last_operator = $last_operator_positions->[$start];
+
+    # Conditions to remove parentheses
+    return 1 if ($start > 0 && $end + 1 < scalar @$expression_chars &&
+        $expression_chars->[$start - 1] eq '(' && $expression_chars->[$end + 1] eq ')');
+    return 1 if (!$operator_map->[ord('+')] && !$operator_map->[ord('*')] &&
+        !$operator_map->[ord('-')] && !$operator_map->[ord('/')]);
+    return 1 if ($last_operator eq '-1' && $next_operator eq '-1');
+    return 1 if ($last_operator eq '-1' && $next_operator eq '/');
+    return 0 if ($start - 1 > 0 && $expression_chars->[$start - 1] eq '+' && $expression_chars->[$start + 1] eq '-');
+    return 1 if ((grep { $_ eq '+' || $_ eq '*' } @$expression_chars[$start .. $end]) &&
+        $expression_chars->[$start - 1] eq '(' && !(grep { $_ eq '-' } @$expression_chars[$start .. $end]));
+
+    if ($last_operator eq '/') {
+        return 0;
+    }
+    elsif ($last_operator eq '-' && ($operator_map->[ord('+')] || $operator_map->[ord('-')])) {
+        return 0;
+    }
+    elsif (!$operator_map->[ord('+')]) {
+        return 1;
+    }
+    elsif (($last_operator eq '-1' || $last_operator eq '+' || $last_operator eq '-') &&
+        ($next_operator eq '-1' || $next_operator eq '+' || $next_operator eq '-')) {
+        return 1;
+    }
+    return 0;
 }
 
 # Build the final result string without unnecessary parentheses
 sub build_result_string {
-    my ($characters, $keep, $length) = @_;
+    my ($characters, $keep_parentheses_ref) = @_;
     my $result = '';
+    my $length = scalar @$characters;
     for (my $i = 0; $i < $length; $i++) {
-        $result .= $characters->[$i] if $keep->[$i];
+        $result .= $characters->[$i] if $keep_parentheses_ref->[$i];
     }
     return $result;
 }
 
 # Main function to remove unnecessary parentheses
 sub remove_parentheses {
-    my ($expression) = @_;
-    my @characters = split //, $expression;
-    my $length = @characters;
+    my @expression_chars = split //, $_[0];
 
-    my @last_operator_start = fill_operator_positions(\@characters, $length, 'start');
-    my @next_operator_end = fill_operator_positions(\@characters, $length, 'end');
+    # Initialize arrays to track parentheses and operators
+    my ($last_operator_positions_ref, $next_operator_positions_ref) = fill_operator_positions(\@expression_chars);
 
-    my @keep = process_parentheses(\@characters, $length, \@last_operator_start, \@next_operator_end);
+    # Process each character in the expression
+    my $keep_parentheses_ref = process_parentheses(\@expression_chars, $last_operator_positions_ref, $next_operator_positions_ref);
 
-    return build_result_string(\@characters, \@keep, $length);
+    # Build and return the result string without unnecessary parentheses
+    return build_result_string(\@expression_chars, $keep_parentheses_ref);
 }
 
 1;
--- ParenthesesRemoval/t/test_remove_parentheses.t
+++ ParenthesesRemoval/t/test_remove_parentheses.t
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use lib qw(lib);
 use ParenthesesRemoval;
-use Test::More tests => 6;
+use Test::More tests => 12;
 
 # Test loading the module
 BEGIN { use_ok('ParenthesesRemoval') }
@@ -19,3 +19,15 @@ is(ParenthesesRemoval::remove_parentheses("x+(y+z)+(t+(v+w))"), "x+y+z+t+v+w", '
 is(ParenthesesRemoval::remove_parentheses("a +  (b * c)"), "a +  b * c", 'Test case 4 failed.');
 # Test the remove_parentheses function, case 5: "-d-e+(f/(g/h))"
 is(ParenthesesRemoval::remove_parentheses("-d-e+(f/(g/h))"), "-d-e+f/(g/h)", 'Test case 5 failed.');
+# Test the remove_parentheses function, case 6: "(2*(3+4)*5)/6"
+is(ParenthesesRemoval::remove_parentheses("(2*(3+4)*5)/6"), "2*(3+4)*5/6", 'Test case 6 failed.');
+# Test the remove_parentheses function, case 7: "(-5)/7"
+is(ParenthesesRemoval::remove_parentheses("(-5)/7"), "-5/7", 'Test case 7 failed.');
+# Test the remove_parentheses function, case 8: "(-5)*7"
+is(ParenthesesRemoval::remove_parentheses("(-5)*7"), "-5*7", 'Test case 8 failed.');
+# Test the remove_parentheses function, case 9: "1+(-1)"
+is(ParenthesesRemoval::remove_parentheses("1+(-1)"), "1+(-1)", 'Test case 9 failed.');
+# Test the remove_parentheses function, case 10: "5*(-3)"
+is(ParenthesesRemoval::remove_parentheses("5*(-3)"), "5*-3", 'Test case 10 failed.');
+# Test the remove_parentheses function, case 11: "((2*((2+3)-(4*6))+(8+(7*4))))"
+is(ParenthesesRemoval::remove_parentheses("((2*((2+3)-(4*6))+(8+(7*4))))"), "2*(2+3-4*6)+8+7*4", 'Test case 11 failed.');
